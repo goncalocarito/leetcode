3ab08188a3a96aed5e3f69ed2746032b290fb9ee
Gonçalo Carito <goncalocarito@gmail.com>
2017-12-15 17:54:40 +0000

    First challenge

---
+++ 414.py
@@ -0,0 +1,44 @@
+# Description: https://leetcode.com/problems/third-maximum-number/description/
+
+# Code
+
+import unittest
+
+
+class Solution:
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        snums = set(nums)
+        firstmax = max(snums)
+        second = snums - set([firstmax])
+        if not second:
+            return firstmax
+        secondmax = max(second)
+        third = snums - set([firstmax]) - set([secondmax])
+        if third:
+            return max(third)
+        else:
+            return firstmax
+
+
+# Tests
+
+class TestSolution(unittest.TestCase):
+    def test_thirdMax1(self):
+        res = Solution().thirdMax([3, 2, 1])
+        self.assertEqual(res, 1)
+
+    def test_thirdMax2(self):
+        res = Solution().thirdMax([1, 2])
+        self.assertEqual(res, 2)
+
+    def test_thirdMax3(self):
+        res = Solution().thirdMax([2, 2, 3, 1])
+        self.assertEqual(res, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
